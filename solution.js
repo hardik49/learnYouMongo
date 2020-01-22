c8fdd42c91abd11bcfcc24d564a79406c4ca7bd7
hardik49 <hardik.thakar49@gmail.com>
2020-01-22 18:27:35 +0530

    Explored Find with  Projection

--- solution.js
+++ solution.js
@@ -3,8 +3,14 @@ const process = require('process');
 const age = parseInt(process.argv[2]);
 let url = "mongodb://localhost:27017/learnyoumongo";
   mongo.connect(url, function(err, db) {
-    let dbo = db.db("learnyoumongo");
+    const dbo = db.db("learnyoumongo");
     (dbo.collection("parrots").find({age:{$gt:+age}})).toArray((err, res) => {
+      if (err) throw err;
+      console.log(res);
+      //dbo.close();
+    })
+
+    (dbo.collection("parrots").find({age:{$gt:+age}},{projection:{name:1, age:1, _id:0}})).toArray((err, res) => {
       if (err) throw err;
       console.log(res);
       dbo.close();
