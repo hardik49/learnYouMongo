10faf51ab1b9b51f769c5f106ae90862b7c8797b
hardik49 <hardik.thakar49@gmail.com>
2020-01-23 09:36:39 +0530

    update data on collection parrots

--- solution.js
+++ solution.js
@@ -4,29 +4,39 @@ const age = parseInt(process.argv[2]);
 let url = "mongodb://localhost:27017/learnyoumongo";
 let firstName = process.argv[2]
 let lastName = process.argv[3]
-  mongo.connect(url, function(err, db) {
-    const dbo = db.db("learnyoumongo");
-    //Explored Find Query
-    (dbo.collection("parrots").find({age:{$gt:+age}})).toArray((err, res) => {
-      if (err) throw err;
-      console.log(res);
-    })    
-    //Explored Projection with Find
-    (dbo.collection("parrots").find({age:{$gt:+age}},{projection:{name:1, age:1, _id:0}})).toArray((err, res) => {
-      if (err) throw err;
-      console.log(res);
-      dbo.close();
-    })
-    //Insert data on Collection Parrots
-    var doc = {
-      firstName: firstName
-    , lastName: lastName
+mongo.connect(url, function(err, db) {
+  const dbo = db.db("learnyoumongo");
+  //Explored Find Query
+  (dbo.collection("parrots").find({age:{$gt:+age}})).toArray((err, res) => {
+    if (err) throw err;
+    console.log(res);
+  })    
+  //Explored Projection with Find
+  (dbo.collection("parrots").find({age:{$gt:+age}},{projection:{name:1, age:1, _id:0}})).toArray((err, res) => {
+    if (err) throw err;
+    console.log(res);
+    dbo.close();
+  })
+  //Insert data on Collection Parrots
+  var doc = {
+    firstName: firstName
+  , lastName: lastName
+  }
+  dbo.collection('users').insert(doc, function(err, data) {
+    if (err) throw err
+    console.log(JSON.stringify(doc))
+    dbo.close()
+  })
+  //Update data on Colllection Parrots
+  dbo.collection("users").update({
+    username: 'tinatime'
+  }, {
+    $set: {
+      age: 40
     }
-
-    dbo.collection('parrots').insert(doc, function(err, data) {
-      if (err) throw err
-      console.log(JSON.stringify(doc))
-      dbo.close()
+  }, function(err) {
+    if (err) throw err
+    db.close()
   })
 })
 
