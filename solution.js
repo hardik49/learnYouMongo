b5dd85e568e45c930c9b3c46075c03a7cd798d2c
hardik49 <hardik.thakar49@gmail.com>
2020-01-23 09:29:12 +0530

    insert data on collection parrots

--- solution.js
+++ solution.js
@@ -2,17 +2,33 @@ var mongo = require('mongodb').MongoClient
 const process = require('process');
 const age = parseInt(process.argv[2]);
 let url = "mongodb://localhost:27017/learnyoumongo";
+let firstName = process.argv[2]
+let lastName = process.argv[3]
   mongo.connect(url, function(err, db) {
     const dbo = db.db("learnyoumongo");
+    //Explored Find Query
     (dbo.collection("parrots").find({age:{$gt:+age}})).toArray((err, res) => {
       if (err) throw err;
       console.log(res);
-      //dbo.close();
-    })
-
+    })    
+    //Explored Projection with Find
     (dbo.collection("parrots").find({age:{$gt:+age}},{projection:{name:1, age:1, _id:0}})).toArray((err, res) => {
       if (err) throw err;
       console.log(res);
       dbo.close();
     })
+    //Insert data on Collection Parrots
+    var doc = {
+      firstName: firstName
+    , lastName: lastName
+    }
+
+    dbo.collection('parrots').insert(doc, function(err, data) {
+      if (err) throw err
+      console.log(JSON.stringify(doc))
+      dbo.close()
+  })
 })
+
+
+
